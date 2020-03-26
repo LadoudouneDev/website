e27a78220d6222e23a1b58c95d1b27ba5e067ac3
Helder <morais.helder@hotmail.fr>
2020-03-26 17:31:35 +0100

    Ajout de la page blog pour les utilisateurs

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="blog", methods={"GET"})
+     */
+    public function index(ArticleRepository $articleRepository): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'articles' => $articleRepository->findAll(),
+        ]);
+    }
+}
