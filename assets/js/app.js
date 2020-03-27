dc42fd900c8e88efa0e3dccc5b6e2d24f2739047
Helder <morais.helder@hotmail.fr>
2020-03-27 17:55:19 +0100

    travaux sur la navbar de la page accueil

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,10 +21,11 @@ $(function(){
     $(window).scroll(function(){
         var winTop = $(window).scrollTop();
         if(winTop >= 30){
-            $("body").addClass("sticky-header");
+            $("body").addClass("sticky-header");    
         }
         else{
             $("body").removeClass("sticky-header");
         }
     });
 });
+
